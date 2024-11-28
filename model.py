7da41c63356793a85da0c4b69afb5346e9ec3247
xdsdmg <2216336843@qq.com>
2024-11-28 23:25:35 +0800

    feat (wip): model

--- model.py
+++ model.py
@@ -31,7 +31,7 @@ class PositionalEncoding(nn.Module):
         return self.dropout(x)
 
 
-def get_pad_mask(seq_q: torch.LongTensor, seq_k: torch.LongTensor):
+def get_pad_mask(seq_q: torch.Tensor, seq_k: torch.Tensor):
     """
     seq_q: batch_size * len_q * d_model
     seq_k: batch_size * len_k * d_model
@@ -65,12 +65,12 @@ def scaled_dot_production_attention(
     attn_mask: torch.Tensor,
 ):
 
-    # batch_size * n_headers * len_q * len_k 
+    # batch_size * n_headers * len_q * len_k
     scores: torch.LongTensor = torch.matmul(Q, K.transpose(-1, -2)) / np.sqrt(D_K)
     scores.masked_fill_(attn_mask, -1e9)
     attn = nn.Softmax(dim=-1)(scores)
-    # (batch_size * n_headers * len_q * len_k) * (batch_size * n_headers * len_k *(d_model/n_heads)) 
-    # -> batch_size * n_headers * len_q * (d_model/n_heads) 
+    # (batch_size * n_headers * len_q * len_k) * (batch_size * n_headers * len_k *(d_model/n_heads))
+    # -> batch_size * n_headers * len_q * (d_model/n_heads)
     context = torch.matmul(attn, V)
     return context
 
@@ -122,3 +122,60 @@ class MultiHeadAttention(nn.Module):
         output = self.concat(context)
 
         return self.layer_norm(output + residual)
+
+
+class PosWiseFFN(nn.Module):
+    def __init__(self):
+        super(PosWiseFFN, self).__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(D_MODEL, D_FF), nn.ReLU(), nn.Linear(D_FF, D_MODEL)
+        )
+        self.layer_norm = nn.LayerNorm(D_MODEL)
+
+    def forward(self, inputs: torch.Tensor):
+        residual = inputs
+        output = self.fc(inputs)
+        return self.layer_norm(output + residual)
+
+
+class EncoderLayer(nn.Module):
+    def __init__(self):
+        super(EncoderLayer, self).__init__()
+        self.self_attn = MultiHeadAttention()
+        self.pos_wise_ffn = PosWiseFFN()
+
+    def forward(self, enc_inputs: torch.Tensor, self_attn_mask: torch.Tensor):
+        enc_outputs = self.self_attn(enc_inputs, enc_inputs, enc_inputs, self_attn_mask)
+        enc_outputs = self.pos_wise_ffn(enc_outputs)
+        return enc_outputs
+
+
+"""
+编码器的主要功能是对输入序列进行特征提取和语义编码。它会将输入序列（例如在自然语言处理中，可能是一个句子）中的每个元素（单词或子词等）转换为一个包含语义信息的向量表示。以处理句子 “我喜欢阅读书籍” 为例，编码器会将每个词映射到一个高维向量空间，这个向量空间中存储了关于这个词的语义信息，同时也包含了它与句子中其他词的关系信息。From Doubao
+"""
+
+
+class Encoder(nn.Module):
+    def __init__(self, src_vocab_size: int):
+        super(Encoder, self).__init__()
+        self.src_emb = nn.Embedding(src_vocab_size, D_MODEL)
+        self.pos_emb = PositionalEncoding(D_MODEL)
+        self.layers = nn.ModuleList([EncoderLayer() for _ in range(N_LAYERS)])
+
+    def forward(self, enc_inputs: torch.Tensor):
+        enc_outputs = self.src_emb(enc_inputs)
+        enc_outputs = self.pos_emb(enc_outputs)
+        self_attn_mask = get_pad_mask(enc_inputs, enc_inputs)
+
+        for layer in self.layers:
+            enc_outputs = layer(enc_outputs, self_attn_mask)
+
+        return enc_outputs
+
+
+class DecoderLayer(nn.Module):
+    def __init__(self):
+        super(DecoderLayer, self).__init__()
+        self.self_attn = MultiHeadAttention()
+        self.enc_self_attn = MultiHeadAttention()
+        self.pos_wise_ffn = PosWiseFFN()
